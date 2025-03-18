fe4c1aeea20c3b79948a80c15cce79405e987c1f
Luka <pablolucasso@icloud.com>
2025-03-18 10:25:47 +0100

    no cache

--- src/app/rezervni-delovi/traktorski-delovi/massey-ferguson/page.tsx
+++ src/app/rezervni-delovi/traktorski-delovi/massey-ferguson/page.tsx
@@ -4,6 +4,8 @@ import ProductsIntro from "@/components/frontend/ProductsIntro";
 import FrontLayout from "@/components/layouts/FrontLayout";
 import { supabase } from "@/lib/supabase";
 
+export const fetchCache = "no-store";
+
 export default async function MasseyFerguson() {
   const { data: products, error } = await supabase
     .from("products")
