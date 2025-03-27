d66ae72cb4c6cc86051dd7ee8fa2cfb501ddce45
Luka <pablolucasso@icloud.com>
2025-03-27 17:36:01 +0100

    no-cache

--- src/app/prikljucne-masine/[category]/page.tsx
+++ src/app/prikljucne-masine/[category]/page.tsx
@@ -4,6 +4,8 @@ import ProductsIntro from "@/components/frontend/ProductsIntro";
 import FrontLayout from "@/components/layouts/FrontLayout";
 import { supabase } from "@/lib/supabase";
 
+export const fetchCache = "force-no-store";
+
 export default async function BrandPage({
     params,
   }: {
