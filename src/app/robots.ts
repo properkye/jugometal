c9844a6f48209163ae4c2d1f025690522939159c
Luka <pablolucasso@icloud.com>
2025-03-27 17:21:46 +0100

    robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots{
             allow:"/",
             disallow:'/admin'
         },
-        sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
+        sitemap: `https://jugometal.vercel.app/sitemap.xml`
     }
 }
