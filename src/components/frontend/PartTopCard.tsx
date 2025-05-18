e056e9ce190a31b5e8dabfc19beec6cdfb4d7a72
properkye <57453277+properkye@users.noreply.github.com>
2025-05-19 01:23:42 +0200

    Update PartTopCard.tsx

--- src/components/frontend/PartTopCard.tsx
+++ src/components/frontend/PartTopCard.tsx
@@ -9,7 +9,6 @@ const PartTopCard:React.FC<PartTopCardProps> = ({title}) => {
       <div className="flex justify-between">
         <div className="">
           <h3 className="font-semibold text-[18px] leading-[26px] text-dark tracking-[-0.4px] md:text-[1.5rem] lg:text-[1.5rem] 2xl:text-[1.5rem]">
-            <span style={{ color: "red" }}>#</span>
             {title}
           </h3>
         </div>
